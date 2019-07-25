dc7642698f7e23eb37a796998549e49bd537c0a5
paulbourceret <paulbourceret@gmail.com>
2019-07-25 18:54:55 +0200

    comment

--- config/db-connection.js
+++ config/db-connection.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 //data import from json file -> uncommented if you want to add new set of data
 
-// const data = require("../data/CityData.json");
-// const Cities = require("../models/Cities");
-// Cities.insertMany(data)
-//   .then(res => console.log(res))
-//   .catch(err => console.log(err));
+const data = require("../data/CityData.json");
+const Cities = require("../models/Cities");
+Cities.insertMany(data)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
 
 mongoose.connect(
   process.env.MONGODB_URI,
